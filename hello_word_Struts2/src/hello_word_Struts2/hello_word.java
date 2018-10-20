244c02443308c04843a15d2a820b1fc0cf077440
Hammouda Abdelfatteh <abdelfatteh1991@gmail.com>
2018-10-20 23:58:27 +0200

    first commit

--- hello_word_Struts2/src/hello_word_Struts2/hello_word.java
+++ hello_word_Struts2/src/hello_word_Struts2/hello_word.java
@@ -2,7 +2,7 @@ package hello_word_Struts2;
 
 public class hello_word {
 public String execute() {
-	System.out.println("on es de le mothode execute");
+	System.out.println("Bonjour tous les monde");
 	return "success";
 }
 }
